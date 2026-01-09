94d309a96043921a94e3dc0ecf67030d1b4c16df
Ngh Tahi <thaingh2402@gmail.com>
2026-01-09 15:07:07 +0700

    commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,23 @@
+import { useEffect, useState } from "react";
 import Category from "../components/Category";
+import categoriesData from "../data/Categories.json";
 
 const HomePage = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    setCategories(categoriesData);
+  }, []);
+
   return (
     <>
       <section className="h-[450px] border-2 border-r-0 border-l-0 border-(--primary-color)">
         Banner
       </section>
+
       <main className="mx-auto -mt-22 max-w-7xl">
-        <section className="">
-          <Category />
+        <section>
+          <Category data={categories} />
         </section>
       </main>
     </>
