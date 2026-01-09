68b9b2fc51da6f95b54dad536d231210af385c29
Hoang Thai <nghthai0224@gmail.com>
2026-01-09 20:05:54 +0700

    commit

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,19 +1,21 @@
+
+
 const Category = ({ data = [] }) => {
   return (
     <div className="flex justify-center gap-10">
       {data.map((item) => (
         <div
-  key={item.id}
-  className="group relative flex h-46 w-46 items-center justify-center overflow-hidden rounded-full border-4 border-(--primary-color) bg-white transition hover:-translate-y-1 hover:shadow-xl"
->
+          key={item.id}
+          className="group relative flex h-46 w-46 items-center justify-center overflow-hidden rounded-full border-4 border-(--primary-color) bg-white transition hover:-translate-y-1 hover:shadow-xl"
+        >
 
           <img
-  src={item.image}
-  alt={item.name}
-  className="h-24 w-24 object-contain transition-transform duration-300 group-hover:scale-110"
-/>
+            src={item.image}
+            alt={item.name}
+            className="h-24 w-24 object-contain transition-transform duration-300 group-hover:scale-110"
+          />
 
-<div className="absolute inset-0 flex translate-y-full items-end justify-center bg-white/80 transition-all duration-300 ease-out group-hover:translate-y-0">
+          <div className="absolute inset-0 flex translate-y-full items-end justify-center bg-white/80 transition-all duration-300 ease-out group-hover:translate-y-0">
             <h3 className="mb-20 font-bold text-(--primary-color)">
               {item.name}
             </h3>
--- src/components/Introduce.jsx
+++ src/components/Introduce.jsx
@@ -3,7 +3,7 @@ import thiepCuoiIMG from '../../public/images/thiep-cuoi.jpg'
 const Introduce = () => {
   return (
     <section className="flex items-center h-[450px] gap-6 border">
-      <div className="w-full">
+      <div className="w-full flex justify-end">
         1
       </div>
 
@@ -11,11 +11,12 @@ const Introduce = () => {
         <img
           src={thiepCuoiIMG}
           alt=""
-          className="h-full w-full"
+          className="w-full h-full object-contain"
         />
+
       </div>
 
-      <div className="w-full">
+      <div className="w-full flex justify-start">
         3
       </div>
     </section>
