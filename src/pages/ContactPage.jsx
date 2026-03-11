f6e227226981d3111763440caa6aa5e36abed517
Hoang Thai <nghthai0224@gmail.com>
2026-03-11 20:08:19 +0700

    commit

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <main className="mx-auto max-w-7xl py-6">
+      <div className="bg-white p-4">Liên hệ</div>
+    </main>
+  );
 };
 
 export default ContactPage;
